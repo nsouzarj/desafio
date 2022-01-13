08a7770581f38017fe5b71a2b97ef791a3b3e2be
nsouzarj <nsouzarj@outlook.com>
2022-01-13 19:18:56 -0300

    Ajustes de código

--- src/main/java/com/oligas/repository/EmbarqueRepository.java
+++ src/main/java/com/oligas/repository/EmbarqueRepository.java
@@ -1,7 +1,6 @@
 package com.oligas.repository;
 
 import com.oligas.models.Embarque;
-import com.oligas.models.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,11 +17,6 @@ public interface EmbarqueRepository extends JpaRepository<Embarque, Integer> {
             "dias_minimo_de_folga " +
             "from embarque where funcionario_id_funcionario=?",nativeQuery = true)
     List<Embarque> findEmbarqueIdFuncionario(Integer id);
-    //Traz a lista de embarque pelas data de embarque.
-    @Query(value = "select id,funcionario_id_funcionario,data_embarque,data_desenbarque,dias_maximo_embarcado, " +
-            "dias_minimo_de_folga  " +
-            "from embarque where data_embarque >= ? and data_embarque <= ? ",nativeQuery = true)
-    List<Embarque> findEmbarqueByDate(Date dataInicial, Date dataFinal);
 
     //Traz a lista de embarque pelas datas de embarque num intervalo.
     @Query(value = "select id,funcionario_id_funcionario,data_embarque,data_desenbarque,dias_maximo_embarcado, " +
@@ -40,8 +34,6 @@ public interface EmbarqueRepository extends JpaRepository<Embarque, Integer> {
             "inner join embarque emb on emb.id=X.id" ,nativeQuery = true)
     Embarque findUltimoEmbarqueFechado(Integer id);
 
-    @Query(value ="select * from (select max(id) as id from embarque where funcionario_id_funcionario=? ) X "+
-            "inner join embarque emb on emb.id=X.id" ,nativeQuery = true)
-    Embarque findUltimoEmbarque(Integer id);
+
 
 }
