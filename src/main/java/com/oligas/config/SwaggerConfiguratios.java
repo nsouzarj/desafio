e2be22a7bea1d2e74b87400d387efe9dbfa489b2
nsouzarj <nsouzarj@outlook.com>
2022-01-15 23:24:35 -0300

    Atualização da documentação do swagger.

--- src/main/java/com/oligas/config/SwaggerConfiguratios.java
+++ src/main/java/com/oligas/config/SwaggerConfiguratios.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,14 +22,15 @@ public class SwaggerConfiguratios extends WebMvcConfigurationSupport {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
 
     }
 
     private ApiInfo apiInfo(){
-        return new ApiInfoBuilder().title("API COM SWAGGER")
-                .description("Documentação da Api")
-                .version("1.0").build();
+        return new ApiInfoBuilder().title("** API DE TESTE **")
+                .description("Api com spring boot para teste.").termsOfServiceUrl("Licença Publica").license("Para uso de teste")
+                .version("1.0").contact(new Contact("Nelson Seixas de souza","https://github.com/nsouzarj","nsouzarj@outlook.com")).
+                build();
     }
 
     @Override
