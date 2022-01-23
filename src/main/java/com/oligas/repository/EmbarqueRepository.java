a3957db867f0b6f042eebe9d5ac3ddcab6b52173
nsouzarj <nsouzarj@outlook.com>
2022-01-23 11:41:23 -0300

    Ajuste no encoder e decoder

--- src/main/java/com/oligas/repository/EmbarqueRepository.java
+++ src/main/java/com/oligas/repository/EmbarqueRepository.java
@@ -1,11 +1,9 @@
 package com.oligas.repository;
-
 import com.oligas.models.Embarque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 
